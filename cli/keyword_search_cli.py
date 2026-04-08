02c34c8a2d89ee4cce8c68558d93401b742ccedd
Adrian Prach <263222609+adrianprach@users.noreply.github.com>
2026-04-08 21:53:39 +0930

    wip: partial matching with tokenization

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,14 +1,39 @@
 import argparse
 import json
+import string
+from stopword import STOPS
+
+
+def erase_stopword(text: list[str]):
+    return list(filter(lambda ele: ele not in STOPS, text))
+
+
+def matching_pipeline(query: str, title: str):
+    query = (query.lower().translate(str.maketrans("", "", string.punctuation))).split(" ")
+    title = (title.lower().translate(str.maketrans("", "", string.punctuation))).split(" ")
+
+    for ele_query in query:
+        if ele_query in title:
+            return True
+        for ele_title in title:
+            if ele_query in ele_title:
+                return True
+    # query = erase_stopword(query)
+    # title = erase_stopword(title)
+    # print("query: ", query, title)
+    # return query in title
+    return False
+
 
 def search(query: str):
-    with open('./data/movies.json', 'r') as f:
-        movies: list = json.load(f)['movies']
-        result = list(filter(lambda m: query in m["title"], movies))
+    with open("./data/movies.json", "r") as f:
+        movies: list = json.load(f)["movies"]
+        result = list(filter(lambda m: matching_pipeline(query, m["title"]), movies))
         sorted_result = sorted(result, key=lambda m: m["id"])[:5]
         for idx, mov in enumerate(sorted_result):
             print(f"{idx + 1}. {mov['title']}")
         print(len(result), len(movies))
+
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
