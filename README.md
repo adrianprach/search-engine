f059a6378ea262a6fe7bc806ee46c84d6dfe5fc3
Adrian Prach <263222609+adrianprach@users.noreply.github.com>
2026-04-09 08:53:22 +0930

    feat: add stemmer

---
+++ README.md
@@ -0,0 +1,3 @@
+## Credits
+
+- [Porter's Suffix Stripping](https://www.cs.toronto.edu/~frank/csc2501/Readings/R2_Porter/Porter-1980.pdf) for stemming
--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,7 +1,19 @@
 import argparse
 import json
 import string
-from stopword import STOPS
+from nltk.stem import PorterStemmer
+
+STOPS = []
+stemmer = PorterStemmer()
+
+
+def define_stopwords():
+    with open("./data/stopwords.txt", "r") as f:
+        return f.read().strip().split("\n")
+
+
+def stem_tokens(text: list[str]):
+    return list(map(lambda token: stemmer.stem(token), text))
 
 
 def erase_stopword(text: list[str]):
@@ -9,8 +21,20 @@ def erase_stopword(text: list[str]):
 
 
 def matching_pipeline(query: str, title: str):
-    query = (query.lower().translate(str.maketrans("", "", string.punctuation))).split(" ")
-    title = (title.lower().translate(str.maketrans("", "", string.punctuation))).split(" ")
+    query = stem_tokens(
+        erase_stopword(
+            (query.lower().translate(str.maketrans("", "", string.punctuation))).split(
+                " "
+            )
+        )
+    )
+    title = stem_tokens(
+        erase_stopword(
+            (title.lower().translate(str.maketrans("", "", string.punctuation))).split(
+                " "
+            )
+        )
+    )
 
     for ele_query in query:
         if ele_query in title:
@@ -18,10 +42,6 @@ def matching_pipeline(query: str, title: str):
         for ele_title in title:
             if ele_query in ele_title:
                 return True
-    # query = erase_stopword(query)
-    # title = erase_stopword(title)
-    # print("query: ", query, title)
-    # return query in title
     return False
 
 
@@ -30,9 +50,10 @@ def search(query: str):
         movies: list = json.load(f)["movies"]
         result = list(filter(lambda m: matching_pipeline(query, m["title"]), movies))
         sorted_result = sorted(result, key=lambda m: m["id"])[:5]
+        stemmed = stem_tokens([query])
         for idx, mov in enumerate(sorted_result):
             print(f"{idx + 1}. {mov['title']}")
-        print(len(result), len(movies))
+        print(stemmed, len(result), len(movies))
 
 
 def main() -> None:
@@ -53,4 +74,5 @@ def main() -> None:
 
 
 if __name__ == "__main__":
+    STOPS = define_stopwords()
     main()
