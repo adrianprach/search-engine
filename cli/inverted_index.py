515b429461d84ca9cf1bc940ef175842aea4b25c
Adrian Prach <263222609+adrianprach@users.noreply.github.com>
2026-04-14 11:02:32 +0930

    feat: add term frequencies and fixed doc_id

--- cli/inverted_index.py
+++ cli/inverted_index.py
@@ -6,19 +6,29 @@ from utils import tokenize
 class InvertedIndex:
     idx_pickel = "./cache/index.pkl"
     docmap_pickel = "./cache/docmap.pkl"
+    term_frequencies_pickel = "./cache/term_frequencies.pkl"
 
     def __init__(self):
         self.index = dict()
         self.docmap = dict()
+        self.term_frequencies = dict()
 
     def __add_document(self, doc_id: int, text: str):
         tokens = tokenize(text, self.stopwords)
+        tf = self.term_frequencies.get(doc_id, dict())
         for idx, token in enumerate(tokens):
             self.index[token] = self.index.get(token, set()).union(set({doc_id}))
+            tf[token] = tf.get(token, 0) + 1
+        self.term_frequencies[doc_id] = tf
 
     def get_documents(self, term: str):
-        print("term", term)
+        # print("term", term)
         return sorted(self.index.get(term.lower(), []))
+
+    def get_tf(self, doc_id, term):
+        doc_terms = self.term_frequencies[doc_id]
+        # print(f"doc_terms: {doc_terms}")
+        return doc_terms.get(term, 0)
 
     def build(self, movies: dict, stopwords: list[str]):
         self.stopwords = stopwords
@@ -26,7 +36,7 @@ class InvertedIndex:
             # text = f"{mov['title']}"
             text = f"{mov['title']} {mov['description']}"
             # print(f"Adding {idx} - {text}")
-            self.__add_document(idx, text)
+            self.__add_document(mov['id'], text)
             self.docmap[idx] = mov
         pass
 
@@ -36,16 +46,25 @@ class InvertedIndex:
             pickle.dump(self.index, file=f, protocol=pickle.HIGHEST_PROTOCOL)
         with open(self.docmap_pickel, 'wb') as f:
             pickle.dump(self.docmap, file=f, protocol=pickle.HIGHEST_PROTOCOL)
+        with open(self.term_frequencies_pickel, 'wb') as f:
+            pickle.dump(self.term_frequencies, file=f, protocol=pickle.HIGHEST_PROTOCOL)
 
     def load(self):
         if not os.path.exists(self.idx_pickel):
             raise Exception(f"File Index: {self.idx_pickel} is not there.")
         if not os.path.exists(self.docmap_pickel):
             raise Exception(f"File Docmap: {self.docmap_pickel} is not there.")
+        if not os.path.exists(self.term_frequencies_pickel):
+            raise Exception(f"File Term frequencies: {self.term_frequencies_pickel} is not there.")
         with open(self.idx_pickel, 'rb') as file:
             self.index = pickle.load(file=file)
         with open(self.docmap_pickel, 'rb') as file:
             self.docmap = pickle.load(file=file)
+        with open(self.term_frequencies_pickel, 'rb') as file:
+            self.term_frequencies = pickle.load(file=file)
+
+
+
         
 
 
--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -24,16 +24,28 @@ def search(query: str):
         results.extend(indexes.get_documents(token))
     for idx, doc_id in enumerate(results[:5]):
         movie = indexes.docmap[doc_id]
-        print(f"{idx+1}. {movie['title']}")
+        print(f"{idx+1}. {movie['title']}, Doc ID: {movie['id']}")
+
+def get_tf(doc_id: int, term: str):
+    if doc_id < 0:
+        raise Exception("Doc_id should not be below 0.")
+    if term is None or len(term) < 1:
+        raise Exception("Search term should not be empty.")
+    indexes = InvertedIndex()
+    indexes.load()
+    print(f"TF: {term}: {indexes.get_tf(doc_id, term.strip().lower())} times.")
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
     subparsers.add_parser("build", help="Build index and docmap for movies")
+    tf_parser = subparsers.add_parser("tf", help="Get term frequencies from specified doc id")
     search_parser = subparsers.add_parser("search", help="Search movies using BM25")
 
     search_parser.add_argument("query", type=str, help="Search query")
+    tf_parser.add_argument("doc_id", type=int, help="Doc id")
+    tf_parser.add_argument("term", type=str, help="term for term frequencies")
 
     args = parser.parse_args()
 
@@ -45,6 +57,8 @@ def main() -> None:
             inverted_index = InvertedIndex()
             inverted_index.build(MOVIES, STOPS)
             inverted_index.save()
+        case "tf":
+            get_tf(args.doc_id, args.term)
         case _:
             parser.print_help()
 
